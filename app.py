264ef12c0a058f1ba0b1bf3eb60165c505d316a3
indieinsight25-netizen <indieinsight25@gmail.com>
2026-02-12 20:19:01 +0700

    Create app.py

---
+++ app.py
@@ -0,0 +1,16 @@
+from fastapi import FastAPI, UploadFile, File
+from fastapi.responses import StreamingResponse
+from rembg import remove
+import io
+
+app = FastAPI()
+
+@app.post("/remove-bg")
+async def remove_bg(file: UploadFile = File(...)):
+    input_data = await file.read()
+    output_data = remove(input_data)
+
+    return StreamingResponse(
+        io.BytesIO(output_data),
+        media_type="image/png"
+    )
